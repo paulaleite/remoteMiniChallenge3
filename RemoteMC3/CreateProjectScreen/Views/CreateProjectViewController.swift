561ee02808fc5936e9a1d9f4c1f9bd7e1541e19c
Cassia <cassia.a.barbosa@gmail.com>
2020-05-14 12:54:58 -0300

    SpecificProjectScreen design incompleted

---
+++ RemoteMC3/CreateProjectScreen/Views/CreateProjectViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CreateProjectViewController.swift
+//  RemoteMC3
+//
+//  Created by Cassia Aparecida Barbosa on 14/05/20.
+//  Copyright © 2020 Paula Leite. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateProjectViewController: UIViewController {
+	
+}
