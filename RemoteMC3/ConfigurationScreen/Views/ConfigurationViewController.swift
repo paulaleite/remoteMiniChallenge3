a79b0f86006a6c84faf429702fc1e1e5d142c980
lffattori <luhf.fattori@icloud.com>
2020-05-15 15:17:26 -0300

    ViewController e CollectionCell da tela de configurações feita com outlets

--- RemoteMC3/ConfigurationScreen/Views/ConfigurationViewController.swift
+++ RemoteMC3/ConfigurationScreen/Views/ConfigurationViewController.swift
@@ -10,5 +10,39 @@ import Foundation
 import UIKit
 
 class ConfigurationViewController: UIViewController {
-    
+    var viewModel: ConfigurationViewModel = ConfigurationViewModel()
+
+    @IBOutlet weak var projectsCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+extension ConfigurationViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getProjectsRowsNumber()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let configurationCollectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "configurationCell", for: indexPath) as? ConfigurationCollectionCell else {
+            return ConfigurationCollectionCell()
+        }
+
+        configurationCollectionCell.nameProject.text = viewModel.projects[indexPath.row].title
+        configurationCollectionCell.responsableProject.text = viewModel.projects[indexPath.row].responsable
+        configurationCollectionCell.phaseProject.text = viewModel.projects[indexPath.row].phase
+
+        configurationCollectionCell.backgroundColor = .white
+        configurationCollectionCell.layer.cornerRadius = 20
+        configurationCollectionCell.layer.shadowColor = UIColor.black.cgColor
+        configurationCollectionCell.layer.shadowOffset = .zero
+        configurationCollectionCell.layer.shadowRadius = 4
+        configurationCollectionCell.layer.shadowOpacity = 0.3
+
+        return configurationCollectionCell
+    }
+
 }
