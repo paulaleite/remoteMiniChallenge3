fe13b27b11af9c1f1f879a6b54b73395ba7a3036
Cassia <cassia.a.barbosa@gmail.com>
2020-05-21 19:03:58 -0300

    reload

--- RemoteMC3/FeedScreen/Views/EmptyCollectionCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EmptyCollectionCell.swift
-//  RemoteMC3
-//
-//  Created by Cassia Aparecida Barbosa on 21/05/20.
-//  Copyright © 2020 Paula Leite. All rights reserved.
-//
-
-import Foundation
